4845e219e5df653bf7506a9afbe05c53f2fbcb34
hyeokjin hong <ghd020411@naver.com>
2024-10-14 14:50:40 +0900

    10.14 실습
    10.14 실습

---
+++ 10.14 실습/소스.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	double d = 0;
+
+	printf("¿øÇÏ´Â ¿¬»êÀ» ¼±ÅÃÇÏ½Ã¿À(1.µ¡¼À 2.»¬¼À 3.°ö¼À 4.³ª´°¼À :\n");
+	scanf_s("%d", &a);
+
+	printf("»çÄ¢¿¬»ê ÀÔ·Â(Á¤¼ö) :\n");
+	scanf_s("%d %d, &b, &c");
+
+	if (a == 1)
+	{
+		printf("%d, b+c");
+	}
+	else if (a == 2)
+	{
+		printf("%d, b-c");
+	}
+	else if (a == 3)
+	{
+		printf("%d, b*c");
+	}
+	else if (a == 4)
+	{
+		d = b / c;
+	}
+
+	printf("°á°ú°ª : %.2lf\n", d);
+
+	return 0;
+}
