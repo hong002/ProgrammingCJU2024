e4d8094988b98bc16344b1ad31bc5793e01a8963
hong002 <ghd020411@naver.com>
2024-10-19 20:31:21 +0900

    Update 소스.cpp

--- Report #2 1번 문제 (switch문)/소스.cpp
+++ Report #2 1번 문제 (switch문)/소스.cpp
@@ -10,16 +10,16 @@ int main(void)
 	printf("판별할 숫자를 입력하시오 : ");                  // "판별할 숫자를 입력하시오 : "를 출력
 	scanf_s("%d", &Number);                                 // 판별할 숫자 Number를 입력 받는다
 
-	Number = Number % 2;
+	Number = Number % 2;                                    // 홀수, 짝수를 구분하기 위해 Number % 2의 결과값을 Number에 저장
 
-	switch (Number)
+	switch (Number)                                         // Number의 값이 얼마인지 확인
 	{
-	case 0:
-		printf(" 해당 숫자는 짝수이다.\n");
+	case 0:                                                 // Number가 0이면, 즉 판별할 숫자가 짝수이면
+		printf(" 해당 숫자는 짝수이다.\n");                 // " 해당 숫자는 짝수이다."를 출력
 		break;
 
-	default:
-		printf(" 해당 숫자는 홀수이다.\n");
+	default:                                                // Number가 0이 아니면, 즉 판별할 숫자가 홀수이면
+		printf(" 해당 숫자는 홀수이다.\n");                 // " 해당 숫자는 홀수이다."를 출력
 		break;
 	}
 
