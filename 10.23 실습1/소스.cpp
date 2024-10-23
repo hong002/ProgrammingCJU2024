277f2af9aa99e2e03c249ed5536d930b54de74ad
hyeokjin hong <ghd020411@naver.com>
2024-10-23 17:49:59 +0900

    본문
    본문

--- 10.23 실습1/소스.cpp
+++ 10.23 실습1/소스.cpp
@@ -3,14 +3,18 @@
 int main(void)
 {
 	int Number;                                                  // 몇단을 계산할지 Number로 표현하겠다
-	int i;
+	int i, j;
 
 	printf("구구단 몇단을 계산할지 입력하시오 : ");
 	scanf_s("%d", &Number);
 
 	for (i = 1; i <= 9; i++)                                    // 9번 반복하여 표현하겠다
 	{
-		printf("%d * %d = %d\n", Number, i, (Number * i));
+		for (j = 1; j <= 9; j++)
+		{
+			printf("%d * %d = %2d\n", j, i, (j * i));
+		}
+		printf("\n");
 	}
 
 	return 0;
